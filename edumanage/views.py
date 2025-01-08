b59b19a61a73e9385da31a01dbc4d4ba36dae5d6
Jamie Getty <jamie.getty@reannz.co.nz>
2025-01-08 13:12:46 +1300

    new: Update import location that changed in Django 2.0

--- edumanage/views.py
+++ edumanage/views.py
@@ -17,7 +17,7 @@ from django.http import (
     HttpResponseBadRequest
 )
 from django.template import RequestContext
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import logout as logout_view
