928fe72814c7d79f56ec66909aafb887f7a221b2
Jamie Getty <jamie.getty@reannz.co.nz>
2025-01-21 10:35:44 +1300

    new: I forgot the default log level is warning

--- djangobackends/shibauthBackend.py
+++ djangobackends/shibauthBackend.py
@@ -17,20 +17,20 @@ class shibauthBackend:
         mail = kwargs.get('mail')
         authsource = kwargs.get('authsource')
         if authsource != 'shibboleth':
-            logger.info(f"Invalid auth source {authsource}")
+            logger.warning(f"Invalid auth source {authsource}")
             return None
 
         try:
             user = self._auth_user(username, firstname, lastname, mail)
         except Exception as e:
-            logger.info("Exception occurred when authenticating user: {e}")
+            logger.warning("Exception occurred when authenticating user: {e}")
             return None
 
         if not user:
-            logger.info(f"Could not authenticate username {username} firstname {firstname} lastname {lastname} mail {mail}")
+            logger.warning(f"Could not authenticate username {username} firstname {firstname} lastname {lastname} mail {mail}")
             return None
 
-        logger.info(f"Authenticated username {username} firstname {firstname} lastname {lastname} mail {mail}")
+        logger.warning(f"Authenticated username {username} firstname {firstname} lastname {lastname} mail {mail}")
         return user
 
     def _auth_user(self, username, firstname, lastname, mail):
@@ -38,7 +38,7 @@ class shibauthBackend:
             user = User.objects.get(username__exact=username)
         # The user did not exist. Create one with no privileges
         except:
-            logger.info(f"Could not find user {username} with mail {mail}")
+            logger.warning(f"Could not find user {username} with mail {mail}")
             user = User.objects.create_user(username, mail, None)
             user.first_name = firstname
             user.last_name = lastname
@@ -47,7 +47,7 @@ class shibauthBackend:
             user.is_active = False
             user.save()
 
-        logger.info(f"Returning user {user}")
+        logger.warning(f"Returning user {user}")
         return user
 
     def get_user(self, user_id):
